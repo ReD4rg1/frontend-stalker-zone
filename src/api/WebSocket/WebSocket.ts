c2ca04d0cbd0f9c957ffcb9658fab1010ccb17a0
ReD4rg1 <musenov200228@gmail.com>
2022-04-12 11:25:12 +0600

    Отрисовал WebSocket Messages

--- src/api/WebSocket/WebSocket.ts
+++ src/api/WebSocket/WebSocket.ts
@@ -1,31 +1,43 @@
 import SockJS from "sockjs-client";
 import {Stomp} from "@stomp/stompjs";
 
-let stompClient: any = null
-const handlers: any[] = []
-
-export function connect() {
-    const socket = new SockJS('http://localhost:8080/initialization-websocket')
-    stompClient = Stomp.over(socket)
-    stompClient.connect({}, (frame: any) => {
-        console.log('Connected: ' + frame)
-        stompClient.subscribe('/characters/list', (char: any) => {
-            handlers.forEach((handler) => handler(JSON.parse(char.body)))
-        })
-    })
+interface CharEffect {
+    id: number
+    name: string
+    value: number
 }
 
-export function addHandler(handler: any) {
-    handlers.push(handler)
+export interface Character {
+    id: number
+    name: string
+    available: boolean
+    effectsList: CharEffect[]
+}
+
+interface Props {
+    setCharacters: (characters: Character[]) => void
+    setConnected: (status: boolean) => void
+}
+
+let stompClient: any = null
+
+export function connect({setCharacters, setConnected}: Props) {
+    const socket = new SockJS('http://localhost:8080/initialization-websocket')
+    stompClient = Stomp.over(socket)
+    stompClient.connect({}, () => {
+        setConnected(true)
+        stompClient.subscribe('/characters/list', (chars: any) => {
+            setCharacters(JSON.parse(chars.body))
+        })
+    })
 }
 
 export function disconnect() {
     if (stompClient !== null) {
         stompClient.disconnect()
     }
-    console.log("Disconnected")
 }
 
-export function sendChar(charId: {id: number}) {
-    stompClient.send("/app/edit", {}, JSON.stringify({charId}))
+export function sendChar(id: number) {
+    stompClient.send("/app/edit", {}, JSON.stringify({id}))
 }
--- src/components/MultiplayerSession/MenuContainer.tsx
+++ src/components/MultiplayerSession/MenuContainer.tsx
@@ -1,23 +1,44 @@
 import {useEffect, useState} from "react";
-import {connect, disconnect, sendChar} from "../../api/WebSocket/WebSocket";
+import {Character, connect, disconnect, sendChar} from "../../api/WebSocket/WebSocket";
+import styles from "./MenuContainer.module.css";
 
 const MenuContainer = () => {
 
-    const [data, setData] = useState({});
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const [connected, setConnected] = useState(false);
 
     useEffect(() => {
-        connect()
+        connect({setCharacters, setConnected})
         return () => disconnect()
     }, [])
 
+    useEffect(() => {
+        if (connected) {
+            sendChar(0)
+        }
+    }, [connected])
+
     return (
         <div>
-            <button onClick={() => sendChar({id: 0})}>
-                Получить
-            </button>
-            <button onClick={() => sendChar({id: 1})}>
-                Поменять
-            </button>
+            <section>
+                {characters.length > 0
+                    ? characters.map((char) => (
+                        <div key={char.id} className={styles.char}>
+                            <div>
+                                <h3>{char.name}</h3>
+                            </div>
+                            <div style={{color: `${char.available ? "white" : "red"}`}}>
+                                {char.available ? "Доступный" : "Недоступный"}
+                            </div>
+                            <div>
+                                <button onClick={() => sendChar(char.id)}>Смена статуса</button>
+                            </div>
+                        </div>
+                        )
+                    )
+                    : <div />
+                }
+            </section>
         </div>
     )
 }
